49ba8ce7b5f6e27f0bf4b752612551cdcc99fe9b
John Running <john.a.running@gmail.com>
2018-12-17 09:17:02 -0500

    updated writereview forms to include word count for heading and fix error messaging on Star Rating

--- project/forms.py
+++ project/forms.py
@@ -60,7 +60,7 @@ class ProductForm(FlaskForm):
 class ReviewForm(FlaskForm):
     heading = StringField('Review Heading',validators=[DataRequired(),Length(min=10, max=50, message='Your heading must be 10 - 50 characters in length.')])
     description = TextAreaField('Review Description',validators=[DataRequired(),Length(min=100, max=2000, message='Your review must be more than 100 characters and less than 2000 characters.')])
-    starrating = HiddenField('Star Rating', validators=[DataRequired()])
+    starrating = HiddenField('Star Rating', validators=[DataRequired(message='You must enter a star rating.')])
     product_id =  HiddenField('Product_ID')
     submit = SubmitField('Submit Review')
 
